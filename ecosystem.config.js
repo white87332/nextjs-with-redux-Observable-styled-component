8993a3df374cd6f4a1c6d49e6b57fce7e2cb294a
Tony Jiang <white87332@gmail.com>
2018-10-16 14:46:17 +0800

    add ecosystem.config.js

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+    apps: [
+        {
+            name: 'app',
+            script: 'server.js',
+            watch: true,
+            ignore_watch: ['/node_moudle'],
+            env: {
+                NODE_ENV: 'production'
+            }
+        }
+    ]
+};
